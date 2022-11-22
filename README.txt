21aab4533b33a8b4a36f0854df0096dcd99d3b6c
dhanno75 <pdhananjay774@gmail.com>
2022-11-22 13:00:15 +0530

    success

--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
-display -> display brand and name of the product, price, image and product link, description
-
 Since the json data from the api is too large the data was taking more than 2 minutes to load the page due to which my browser got stuck and my system also got slow. The other issue was that some of the images was not loading. I raised my query in zen portal and the mentor 'Suman Gangopadhyay' assigned to me said that copy the json from the api to a local file and then use it. He said use a default image which says 'no image found' if the images is not loading.
 
 Copying the data to the local file did not help, so I restricted the array of objects data to just 200.
+
+I am taking the data from index 100 to 300 because 1st 100 data has almost no images though I have the solution for it(which I already mentioned)
--- script.js
+++ script.js
@@ -3,26 +3,23 @@ document.body.innerHTML = `
   <div class='text-center'>
     <h1 id="title" class="text-center mt-4">Makeup brands</h1>
   </div>
-  <input
-    type="search"
-    class="search-field form-control mt-5"
-    placeholder="Search your product"
-  />
+    <input type="search" class="form-control rounded search-field" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
   </div>
   <div class="main-container"></div>
 `;
 
-// {
-//   <i class="bi bi-link"></i>
-// }
+let makeupStore = [];
+
 // Get the makeup details
 const getMakeupDetails = async () => {
   try {
     let data = await fetch("./makeup.json");
     let makeupProducts = await data.json();
     for (let i = 100; i < 300; i++) {
+      makeupStore.push(makeupProducts[i]);
       renderMakeupProducts(makeupProducts[i]);
     }
+    console.log(makeupStore);
   } catch (err) {
     console.log(err);
   }
@@ -59,18 +56,73 @@ const renderMakeupProducts = (makeup) => {
   `;
 };
 
-let searchField = document.querySelector(".search-field");
-searchField.addEventListener("keyup", (e) => {
+let search = document.querySelector(".search-field");
+search.addEventListener("keyup", (e) => {
   e.preventDefault();
-  let textSearched = searchField.value;
-  let mainCont = document.querySelector(".main-container");
+  let searchField = document.querySelector(".search-field").value;
+  let filteredData = [];
 
-  textSearched = textSearched.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  if (searchField !== "") {
+    filteredData = makeupStore.filter(
+      (makeup) =>
+        makeup.brand.toLowerCase().includes(searchField.toLowerCase()) ||
+        makeup.name.includes(searchField.toLowerCase())
+    );
 
-  let pattern = new RegExp(`${textSearched}`, "gi");
-
-  mainCont.innerHTML = mainCont.replace(
-    pattern,
-    (match) => `<mark>${match}</mark>`
-  );
+    //
+    if (filteredData.length !== 0) {
+      document.querySelector(".main-container").innerHTML = "";
+    }
+    for (let data of filteredData) {
+      renderSearchData(data, searchField);
+    }
+  } else {
+    alert("No such makeup brand found!");
+  }
 });
+
+const renderSearchData = (makeup, textEntered) => {
+  let mainContainer = document.querySelector(".main-container");
+  // mainContainer.innerHTML = "";
+  mainContainer.innerHTML += `
+  <div class="container">
+    <h3 class="brand">${
+      makeup.brand !== textEntered
+        ? makeup.brand
+        : `<mark>${makeup.brand}</mark>`
+    }</h3>
+    <div class="image">
+      <img
+        src="${makeup.image_link}"
+        onerror="this.src='./img/noimage.jpg'"
+        alt="${makeup.name}"
+        class="image-icon"
+      />
+    </div>
+    <div class="nameLink">
+    <q class="name">${
+      makeup.name !== textEntered ? makeup.name : `<mark>${makeup.name}</mark>`
+    }</q>
+    <a href="${
+      makeup.product_link
+    }" class="plink" target='_blank'>Buy now <i class="bi bi-box-arrow-up-right"></i></a>
+      </div>
+      <p class="price">${makeup.price_sign}${makeup.price}</p>
+      <div class="descbox">
+      <p class='about'>Description:</p>
+      <p class="description">${
+        makeup.description ? makeup.description : "No description found!"
+      }</p>
+    </div>
+  </div>
+  `;
+};
+
+// searchedText = searchedText.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+// let pattern = new RegExp(`${searchedText}`, "gi");
+
+// mainCont.innerHTML = mainCont.textContent.replace(
+//   pattern,
+//   (match) => `<mark>${match}</mark>`
+// );
